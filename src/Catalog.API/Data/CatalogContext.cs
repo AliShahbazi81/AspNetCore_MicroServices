110e0d9c9072d234a36b3232545c810cfc50d09d
Ali Shahbazi <ali.vortex799@gmail.com>
2023-12-19 14:44:30 -0500

    Create Catalog context and seed Products

---
+++ src/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IConfiguration configuration)
+    {
+        // Initiate the connection to the database
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        // Find and get the database, if null -> create
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("Database:CollectionName"));
+        CatalogContextSeed.SeedData(Products);
+    }
+    public IMongoCollection<Product> Products { get; }
+}
---
+++ src/Catalog.API/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities;
+
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required string Category { get; set; }
+    public string? Summary { get; set; }
+    public string? Description { get; set; }
+    public string? ImageFile { get; set; }
+    public decimal Price { get; set; }
+}
