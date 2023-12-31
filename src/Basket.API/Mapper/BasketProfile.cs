e776b21d3e5f51ebe663392c912232cbb6b5aa7f
Ali Shahbazi <ali.vortex799@gmail.com>
2023-12-31 16:31:32 -0500

    Basket auto mapper

---
+++ src/Basket.API/Mapper/BasketProfile.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Mapper;
+
+public class BasketProfile
+{
+    public BasketProfile()
+    {
+        CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+    }
+}
